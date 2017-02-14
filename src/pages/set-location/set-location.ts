85b9d4896f044bddc7cc8ccccb5c706132201f8b
Techiebud (Mac) <techiebud@gmail.com>
2017-02-13 20:31:03 -0500

    Lesson 174

--- src/pages/set-location/set-location.ts
+++ src/pages/set-location/set-location.ts
@@ -7,11 +7,21 @@ import { NavParams } from 'ionic-angular';
   templateUrl: 'set-location.html'
 })
 export class SetLocationPage {
-  location: Location
+  location: Location;
+  marker: Location;
 
   constructor(private navParams: NavParams) {
      this.location = this.navParams.get("location");
 
+  }
+
+  onSetMarker(event: any) {
+     console.debug("event");
+     console.log(event);
+     this.marker = new Location(event.coords.lat, event.coords.lng);
+        
+    
+      
   }
       
 
