d179aa078535ec1ad56f71d077de8a6f47962be2
Techiebud (Mac) <techiebud@gmail.com>
2017-02-14 20:11:48 -0500

    Lesson 184

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -1,4 +1,4 @@
-import { Camera, Geolocation } from 'ionic-native';
+import { Camera, Entry, File, FileError, Geolocation } from 'ionic-native';
 import { LoadingController, ModalController, ToastController } from "ionic-angular";
 
 import { Component } from '@angular/core';
@@ -7,6 +7,7 @@ import { NgForm } from "@angular/forms";
 import { PlacesService } from './../../services/places';
 import { SetLocationPage } from './../set-location/set-location';
 
+declare var cordova: any;
 @Component({
   selector: 'page-add-place',
   templateUrl: 'add-place.html'
@@ -20,10 +21,10 @@ export class AddPlacePage {
   location: Location = this.defaultLocation;
   locationIsSet = false;
   imageUrl: string = '';
-  constructor(private modalCtrl: ModalController, 
-   private loadingCtrl: LoadingController,
-   private toastCtrl: ToastController,
-   private placesService: PlacesService ) { }
+  constructor(private modalCtrl: ModalController,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController,
+    private placesService: PlacesService) { }
 
   onSubmit(form: NgForm) {
     console.debug("onSubmit");
@@ -36,14 +37,14 @@ export class AddPlacePage {
   }
 
   onOpenMap() {
-    
+
     const modal = this.modalCtrl.create(SetLocationPage, { location: this.location, isSet: this.locationIsSet });
     modal.present();
 
     modal.onDidDismiss(data => {
       if (data) {
-          this.location = data.location;
-          this.locationIsSet = true;
+        this.location = data.location;
+        this.locationIsSet = true;
 
       }
     }
@@ -56,61 +57,92 @@ export class AddPlacePage {
 
   onLocate() {
     console.debug("onLocate");
-      const loader = this.loadingCtrl.create({
-          content: 'Getting your location.....'
-      });
-      loader.present();
-      Geolocation.getCurrentPosition()
-          .then(
-             location => {
-               // console.log(location);
-               loader.dismiss();
-               this.location.lat = location.coords.latitude;
-               this.location.lng = location.coords.longitude;
-               this.locationIsSet = true;
-                
-               
-             }
+    const loader = this.loadingCtrl.create({
+      content: 'Getting your location.....'
+    });
+    loader.present();
+    Geolocation.getCurrentPosition()
+      .then(
+      location => {
+        // console.log(location);
+        loader.dismiss();
+        this.location.lat = location.coords.latitude;
+        this.location.lng = location.coords.longitude;
+        this.locationIsSet = true;
 
-          )
-          .catch(
-          
-              error => {
-                loader.dismiss();
-                const toast = this.toastCtrl.create({
-                   message: 'Sorry, could not get your location',
-                   duration: 2500
-                });
-                toast.present();
-                console.log(error);
+
+      }
+
+      )
+      .catch(
+
+      error => {
+        loader.dismiss();
+        const toast = this.toastCtrl.create({
+          message: 'Sorry, could not get your location',
+          duration: 2500
+        });
+        toast.present();
+        console.log(error);
+      }
+
+      );
+
+
+  }
+
+  onTakePhoto() {
+
+    Camera.getPicture({
+      encodingType: Camera.EncodingType.JPEG,
+      correctOrientation: true
+
+    })
+      .then(
+      imageData => {
+
+        const currentName = imageData.replace(/^.*[\\\/]/, ''); //strip out path
+        const path = imageData.replace(/[^\/]*$/, '');  //strip out file name
+        File.moveFile(path, currentName, cordova.file.dataDirectory, currentName)
+          .then(
+              (data: Entry) => {
+
+                this.imageUrl = data.nativeURL;
+                Camera.cleanup();
+
               }
 
-          );
-
-          
-      }
-
-      onTakePhoto() {
-
-          Camera.getPicture({
-            encodingType: Camera.EncodingType.JPEG,
-            correctOrientation: true
-            
-          })
-          .then(
-            imageData => {
-              console.log(imageData);
-              this.imageUrl = imageData;
-            }
           )
           .catch(
-            error => {
-              console.log(error);
-            }
+         (error: FileError) => {
+            console.log(error.message);
+            this.imageUrl = '';
+            const toast = this.toastCtrl.create({
+
+              message: 'Could not save the image.    Please try again',
+              duration: 2500
+            })
+            toast.present();
+            Camera.cleanup();
+          }
           )
+        console.log(imageData);
+        this.imageUrl = imageData;
       }
-    
+      )
+      .catch(
+      error => {
+        console.log(error);
+        const toast = this.toastCtrl.create({
+
+          message: 'Could not take the image.    Please try again',
+          duration: 2500
+        })
+      }
+      )
   }
+
+}
 
 
 
